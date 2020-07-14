08a1d72085e63833a3025d422b5023e815690c00
ShEv <es@admitad.com>
2020-07-14 13:52:45 +0300

    add fix

--- get_data.py
+++ get_data.py
@@ -4,7 +4,7 @@ from loger import create_loger
 from settings import key
 
 
-def get_all_photo_func() -> list:
+def get_all_photo_func() -> dict:
     """
     Функция запроса всех фотографий с вашей страницы
     :key - ключ авторизации для вашего приложения, должен быть в окружении.
@@ -13,16 +13,16 @@ def get_all_photo_func() -> list:
     """
     error_log = create_loger(get_all_photo_func.__name__)
     error_log.debug('Starting to collect data from function')
-    for url in ['https://api.vk.com/method/photos.getAll?v=5.52&access_token=' + str(key)]:
-        try:
-            response = requests.get(url)
-            response.raise_for_status()
-        except HTTPError as http_err:
-            error_log.error(f'Function failed because of this: {http_err}')
-        except Exception as err:
-            error_log.error(f'Function failed because of this: {err}')
-        else:
-            print('Success!')
-            error_log.debug('Requested successfully')
-        res: dict = response.json()
-        return res['response']['items']
+    url: str = 'https://api.vk.com/method/photos.getAll?v=5.52&access_token=' + str(key)
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+    except HTTPError as http_err:
+        error_log.error(f'Function failed because of this: {http_err}')
+    except Exception as err:
+        error_log.error(f'Function failed because of this: {err}')
+    else:
+        print('Success!')
+        error_log.debug('Requested successfully')
+    res: dict = response.json()
+    return res['response']['items']
